0651ffa9fe51c46ba8ae4318fd1749e99c05e698
EmersonBLopes <emersonbasiliolopes@gmail.com>
2023-04-28 23:35:23 -0300

    Conclusao exercicioI

---
+++ Linguagem_C/Algoritmos/LogicaParaDesenvolvimentoDeProgramacaoDeComputadores/capitulo_5/exercicio5I.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(void){
+  //declaracao de variaveis
+  int anterior,atual,proximo,i;
+  anterior = 0;
+  atual = 1;
+  i = 1;
+  
+  //processamento e saida
+  while(i <= 15){
+
+    proximo = anterior + atual;
+
+    //saida
+    if(i != 15){
+      printf("%d ",atual);
+    }else{
+      printf("%d\n",atual);
+    }
+
+    anterior = atual;
+    atual = proximo;
+    i++;
+  }
+  
+  
+  
+  return 0;
+}
