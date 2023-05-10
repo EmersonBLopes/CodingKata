03656999cdcb7fc6a7157b73886968591c2f0141
EmersonBLopes <emersonbasiliolopes@gmail.com>
2023-05-09 23:19:46 -0300

    Conclusao do exercicio5K utilizando o laco de repeticia pos teste do while

---
+++ Linguagem_C/Algoritmos/LogicaParaDesenvolvimentoDeProgramacaoDeComputadores/capitulo_5/do_while/exercicio5K.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<limits.h>
+
+/**
+ * Calcular e apresentar o somatorio dos graos em um tabuleiro de xadrez
+ */
+
+int main(void){
+  
+  //Declaracao e inicializacao de variaveis
+  int i = 0;
+  unsigned long long graosCasaAtual, somatorioDeGraos;
+  graosCasaAtual = 0;
+  somatorioDeGraos = 0;
+
+  //processamento
+  do{
+    graosCasaAtual = i == 0 ? 1 : graosCasaAtual * 2;
+    somatorioDeGraos += graosCasaAtual;
+    i++;
+  }while(i < 65);
+
+  //saida
+  printf("O somatorio de graos no tabuleiro eh: %llu\n",somatorioDeGraos);
+  
+  return 0;
+}
