9f6ff959a764c0ce0df177369b7deed5dcb9bec5
EmersonBLopes <emersonbasiliolopes@gmail.com>
2023-05-14 20:49:12 -0300

    Conclusao do exercicio5B utilizando o laco de repeticao for

---
+++ Linguagem_C/Algoritmos/LogicaParaDesenvolvimentoDeProgramacaoDeComputadores/capitulo_5/for/exercicio5B.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ *Apresetar a tabuada de um numero qualquer em sua forma tradicional
+**/
+
+int main(void){
+	
+	//declaracao de variaveis
+	int valor;
+
+	//leitura de valores
+	printf("Digite o valor que deseja consultar a tabuada:");
+	scanf("%d",&valor);
+
+	//processamento e saida
+	for(int i = 0; i < 11; i++){
+			printf("%d X %d = %d\n",valor,i,valor*i);
+	}
+	
+	return 0;
+}
+
