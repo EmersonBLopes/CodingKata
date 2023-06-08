e853741bc40b820f496ae8ad372650a55d6d7d8a
EmersonBLopes <emersonbasiliolopes@gmail.com>
2023-06-07 22:58:20 -0300

    Conclusao do exercicio3A utilizando a linguagem python

---
+++ Python/Algoritmos/LogicaParaDesenvolvimentoDeComputadores/Capitulo_3/exercicio3A.py
@@ -0,0 +1,7 @@
+"""
+    ler uma temperatura em graus celsius e apresenta-la em graus fahrenheit
+"""
+
+celsius = int(input("Digite a temperatura em graus celsius:"))
+fahrenheit = (9 * celsius + 160) / 5
+print(f'{fahrenheit} F');
