5a4c4183607dc3554a08fd859fcece29e5b6cc13
EmersonBLopes <emersonbasiliolopes@gmail.com>
2023-06-11 22:59:47 -0300

    Conclusao do exercicio3G utilizando a linguagem de programacamo python

---
+++ Python/Algoritmos/LogicaParaDesenvolvimentoDeComputadores/Capitulo_3/exercicio3G.py
@@ -0,0 +1,23 @@
+"""
+Elaborar um programa que efetue operacoes de adicoes e multiplicacoes utilizando o conceito de propriedade distribuitiva 
+"""
+
+a = int(input("Digite o valor de a:"))
+b = int(input("Digite o valor de b:"))
+c = int(input("Digite o valor de c:"))
+d = int(input("Digite o valor de d:"))
+
+print(f"A + B = {a+b}")
+print(f"A + C = {a+c}")
+print(f"A + D = {a+d}")
+print(f"B + C = {b+c}")
+print(f"B + D = {b+d}")
+print(f"C + D = {c+d}")
+
+print(f"A * B = {a*b}")
+print(f"A * C = {a*c}")
+print(f"A * D = {a*d}")
+print(f"B * C = {b*c}")
+print(f"B * D = {b*d}")
+print(f"C * D = {c*d}")
+
